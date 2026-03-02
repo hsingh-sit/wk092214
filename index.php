c417bfd87de3597ea347e8d5ad7c940d071e9a80
hsingh-sit <harminder.singh@singaporetech.edu.sg>
2026-03-03 02:53:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Week 09 Lab - Session Management </h1>
+<ul>
+<li><a href="chase.php">Time-related token</a></li>
+<li><a href="structured.php">Structured token</a></li>
+<li><a href="encrypted.php">Encrypted token Auth</a></li>
+<li><a href="mainpage.php">Session Cryptography Analysis</a></li>
+</ul>
+</body>
+</html>
